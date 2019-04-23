007aaa469b471ae16ed954339e57e6fe9c30975d
Harsh Dineshkumar Shah <48324810+harsh26shah03@users.noreply.github.com>
2019-04-23 22:10:04 +0530

    Add files via upload

---
+++ PageRank.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy as sc
+import pandas as pd
+from fractions import Fraction
+ 
+def float_format(vector, decimal):
+    return np.round((vector).astype(np.float), decimals=decimal)
+
+dp = Fraction(1,3)
+ 
+M = np.matrix([[0,0,1],
+        [Fraction(1,2),0,0],
+        [Fraction(1,2),1,0]])
+ 
+E = np.zeros((3,3))
+E[:] = dp
+ 
+
+beta = 0.7
+ 
+A = beta * M + ((1-beta) * E)
+ 
+
+r = np.matrix([dp, dp, dp])
+r = np.transpose(r)
+ 
+previous_r = r
+for it in range(1,100):
+    r = A * r
+    print (float_format(r,3))
+    if (previous_r==r).all():
+        break
+    previous_r = r
+ 
+print ("Final:\n", float_format(r,3))
+print ("sum", np.sum(r))
